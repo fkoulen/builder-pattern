fb5827b9be33219c1eab77277d1a0ad31f8cb157
F.S. Koulen <feryllia.koulen@hva.nl>
2023-02-15 23:59:02 +0100

    Add code that states problems

--- src/main/java/org/koulenf/Car.java
+++ src/main/java/org/koulenf/Car.java
@@ -1,4 +1,60 @@
 package org.koulenf;
 
 public class Car {
+    private int id;
+    private String brand;
+    private String model;
+    private String color;
+    private double weight;
+    private double height;
+    private int numberOfDoors;
+    private String engine;
+
+    public Car(int id, String brand, String model, String color, double weight, double height, int numberOfDoors, String engine) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.weight = weight;
+        this.height = height;
+        this.numberOfDoors = numberOfDoors;
+        this.engine = engine;
+    }
+
+
+    public void drive() {}
+    public void brake() {}
+    public void turnOnAC() {}
+
+    // Problem: Too Many Constructors
+    public Car(int id, String brand, String model) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public Car(int id, String brand, String model, String color) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+    }
+
+    public Car(int id, String brand, String model, String color, String engine) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.engine = engine;
+    }
+
+    public Car(int id, String brand, String model, String color, double weight, double height) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.weight = weight;
+        this.height = height;
+    }
+
 }
--- src/main/java/org/koulenf/Main.java
+++ src/main/java/org/koulenf/Main.java
@@ -1,7 +1,31 @@
 package org.koulenf;
 
+/**
+ * The builder pattern:
+ *  - creational design pattern: about the way of creating objects
+ *  - Separates (complex) construction code from logic of class
+ */
 public class Main {
     public static void main(String[] args) {
+        int id = 1;
+        String brand = "Suzuki";
+        String model = "Swift";
+        String color = "Red";
+        double weight = 988.80;
+        double height = 2.3;
+        int numberOfDoors = 5;
+        String engine = "1298 cc";
+
+
+        // Problem: Big constructor
+        Car carBigConstructor = new Car(id, brand, model, color, weight, height, numberOfDoors, engine);
+
+        // Problem: Value of null
+        Car carOptional = new Car(id, brand, model, null, weight, height, numberOfDoors, null);
+
+
+
+
 
         Director director = new Director();
         CarBuilder builder = new CarBuilder();
